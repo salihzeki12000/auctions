71846823c9661163e8e533d813ab76db480a7a25
Julian Herrero <microweb10@gmail.com>
2018-06-21 20:27:28 +0700

    deploy with capistrano

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,22 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => "urlmini.microweb10.com" }
+
+		# Mailer options
+		config.action_mailer.perform_deliveries = true
+		config.action_mailer.raise_delivery_errors = true
+		config.action_mailer.delivery_method = :smtp
+		config.action_mailer.smtp_settings = {
+    :address														=> "smtp.gmail.com",
+				:port																	=> 587,
+				:domain															=> "urlmini.microweb10.com",
+				:user_name												=> ENV["EMAIL_USER"],
+				:password													=> ENV["EMAIL_PASSWORD"],
+				:authentication							=> "plain",
+				:enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
